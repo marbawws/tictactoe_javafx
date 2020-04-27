deeb9cdbc122b8d59508a586c22e757e98013d12
Marwane Rezgui <clibeau2010@hotmail.com>
2020-04-27 14:57:01 -0400

    Le chemin du fichier de traduction était mal écrit dans le chargeur

--- src/main/java/tictactoe_javafx/Principal.java
+++ src/main/java/tictactoe_javafx/Principal.java
@@ -42,7 +42,7 @@ public class Principal extends Application {
 		
 		ChargeurDeVue<VuePrincipalFX> chargeur;
 	    chargeur = new ChargeurDeVue<VuePrincipalFX>("/fxml/principal.xml",
-	                                                  "traductios.chaines",
+	                                                  "traductions.chaines",
 	                                                  "/css/principal.css");
 	    
 	    Scene scene = chargeur.nouvelleScene(800, 600);
